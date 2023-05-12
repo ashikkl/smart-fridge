31ca0fed7fec668e99a8dfcd0b924135fc49b8c4
Ashik K L <ashikkl19112001@gmail.com>
2023-05-12 22:23:21 +0530

    add: not-found metadata

--- src/app/[...not-found]/page.tsx
+++ src/app/[...not-found]/page.tsx
@@ -1,5 +1,9 @@
 import { notFound } from "next/navigation";
 
+export const metadata = {
+  title: "Smart Fridge | Page not found",
+  description: "Smart Fridge app to monitor the fridge remotely.",
+};
 function page() {
   return notFound();
 }
