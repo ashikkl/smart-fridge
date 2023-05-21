8bee9d7831c3b20687dac45bacd702802c7a14b9
Ashik K L <ashikkl19112001@gmail.com>
2023-05-21 17:46:00 +0530

    add: dashboard nav

--- src/components/DashNavbar.tsx
+++ src/components/DashNavbar.tsx
@@ -1,7 +1,49 @@
 "use client";
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
+
 function DashNavbar() {
-  return <div className="relative w-[100%] flex flex-row gap-4">DashNavbar</div>;
+  const pathname = usePathname();
+  const hrefs = [
+    { title: "Food Items", link: "/dashboard/fooditems" },
+    { link: "/dashboard/inside", title: "Inside" },
+    { link: "/dashboard/recipes", title: "Recipes" },
+  ];
+
+  const makeLink = ({
+    title: title,
+    link: link,
+  }: {
+    title: string;
+    link: string;
+  }) => {
+    const isActive = pathname.startsWith(link);
+
+    return (
+      <Link
+        href={link}
+        className={
+          isActive
+            ? "text-3xl font-extrabold text-slate-800 hover:text-slate-500 dark:text-white dark:hover:text-slate-400"
+            : "text-base text-slate-800/50 hover:text-slate-500/50 dark:text-white/50 dark:hover:text-slate-400/50"
+        }
+        key={title}
+      >
+        {title}
+      </Link>
+    );
+  };
+  return (
+    <motion.div
+      layout
+      transition={{ layout: { duration: 0.5 } }}
+      className="relative flex w-[100%] flex-row items-end gap-4 antialiased"
+    >
+      {hrefs.map((href) => makeLink(href))}
+    </motion.div>
+  );
 }
 
 export default DashNavbar;
