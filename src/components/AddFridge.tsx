f2866ad92a7f61ad6dbbacad97d9e0d855bc9c4d
Ashik K L <ashikkl19112001@gmail.com>
2023-05-23 01:21:21 +0530

    add: fridge selector functionality and zustand state management

---
+++ src/components/AddFridge.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import { Button } from "@/components/ui/Button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/Dialog";
+import { Input } from "@/components/ui/Input";
+import { Label } from "@/components/ui/Label";
+import React from "react";
+import { useFridgeStore } from "./FridgeSelector";
+
+export function AddFridge(): React.JSX.Element {
+  const [open, setOpen] = React.useState(false);
+  const [id, setId] = React.useState("");
+  const [name, setName] = React.useState("");
+  const { addFridge } = useFridgeStore();
+  async function addFridgeToStore(id: string, name: string): Promise<void> {
+    await addFridge(id, name);
+  }
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <button className="flex flex-row items-center">
+          <Plus /> Add a Fridge
+        </button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add a Fridge</DialogTitle>
+          <DialogDescription>
+            Link a fridge to your account here. Click save when you`&apos;`re
+            done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Fridge ID
+            </Label>
+            <Input
+              id="name"
+              onChange={(e) => {
+                setId(e.target.value);
+              }}
+              value={id}
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Fridge Name
+            </Label>
+            <Input
+              id="username"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              value={name}
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button
+            onClick={(event) => {
+              addFridgeToStore(id, name).then(() => setOpen(false));
+              event.preventDefault();
+            }}
+          >
+            Save changes
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/components/FridgeSelector.tsx
+++ src/components/FridgeSelector.tsx
@@ -1,5 +1,6 @@
+"use client";
+
 import * as React from "react";
-import { Plus } from "lucide-react";
 import {
   Select,
   SelectContent,
@@ -10,36 +11,97 @@ import {
   SelectValue,
   SelectSeparator,
 } from "@/components/ui/Select";
+import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
+import { AddFridge } from "./AddFridge";
+import { Trash } from "lucide-react";
+import { Button } from "./ui/Button";
+
+interface Fridge {
+  id: string;
+  fridgeName: string;
+}
+interface FridgeStore {
+  fridge?: string;
+  fridges: Fridge[];
+  update: (defaultFridge: string) => void;
+  addFridge: (id: string, fridgeName: string) => void;
+  removeFridge: (id: string) => void;
+}
+
+export const useFridgeStore = create<FridgeStore>()(
+  devtools(
+    persist(
+      (set) => ({
+        fridge: "",
+        fridges: [],
+        update: (defaultFridge) => set(() => ({ fridge: defaultFridge })),
+
+        addFridge: (id, fridge) =>
+          set((state) => ({
+            fridges: [
+              ...state.fridges,
+              { id: id, fridgeName: fridge } as Fridge,
+            ],
+          })),
+
+        removeFridge: (id) => {
+          set((state) => ({
+            fridges: state.fridges.filter((fridge) => fridge.id !== id),
+          }));
+        },
+      }),
+      {
+        name: "FridgeStore",
+      }
+    )
+  )
+);
 
 export function FridgeSelector() {
+  let { fridges, fridge, update, removeFridge } = useFridgeStore();
+
   return (
-    <Select>
-      <SelectTrigger className="w-[100%] md:w-[180px]">
-        <SelectValue placeholder="Select a Fridge" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Connected Fridges</SelectLabel>
-          <SelectItem value="Fridge 1">Fridge 1</SelectItem>
-          <SelectSeparator />
-          <SelectLabel
-            onClick={() => {
-              addFridge();
-            }}
-          >
-            <div className="flex flex-row items-center gap-1">
-              <Plus />
-              <button>Add a Fridge</button>
-            </div>
-          </SelectLabel>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+    <div className="flex flex-row items-center gap-4">
+      <Button
+        variant="ghost"
+        size="sm"
+        onClick={() => {
+          removeFridge(fridge ? fridge : "");
+        }}
+      >
+        <Trash width={15} />
+      </Button>
+      <Select
+        /* defaultValue={fridge} */
+        onValueChange={(e) => {
+          update(e);
+        }}
+      >
+        <SelectTrigger className="w-[100%] md:w-[180px]">
+          <SelectValue placeholder="Select a Fridge" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Connected Fridges</SelectLabel>
+            {fridges.map((fridge) => {
+              return (
+                <SelectItem key={fridge.id} value={fridge.id}>
+                  <div className="flex flex-row items-center gap-2">
+                    {fridge.fridgeName}
+                  </div>
+                </SelectItem>
+              );
+            })}
+            <SelectSeparator />
+            <SelectLabel>
+              <div className="flex flex-row items-center gap-1">
+                <AddFridge />
+              </div>
+            </SelectLabel>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
   );
 }
-//TODO add functionality
-const addFridge = () => {
-  return "to do";
-};
-
-export default addFridge;
