5cb485671fbef25acec66e3aefd982669c877d0b
Ashik K L <ashikkl19112001@gmail.com>
2023-05-25 21:57:34 +0530

    fix: output type

--- src/components/FoodItems.tsx
+++ src/components/FoodItems.tsx
@@ -3,14 +3,24 @@
 import FoodItemCard from "./FoodItemCard";
 import { FoodItem, useFoodItemStore } from "./FoodItemsStore";
 
-function FoodItems() : JSX.Element | JSX.Element[] {
-    const { foodItems }: { foodItems: FoodItem[]; } = useFoodItemStore();
-    if (foodItems && foodItems.length > 0) {
-        return foodItems.map((foodItem) => {
-            return <FoodItemCard key={foodItem.foodItemName} title={foodItem.foodItemName} dateCreated={foodItem.dateAdded} />;
-        });
-    }
-    return <div>No food items detected</div>;
+function FoodItems(): JSX.Element {
+  const { foodItems }: { foodItems: FoodItem[] } = useFoodItemStore();
+  if (foodItems && foodItems.length > 0) {
+    return (
+      <div className="flex flex-col items-center w-full justify-start ">
+        {foodItems.map((foodItem): JSX.Element => {
+          return (
+            <FoodItemCard
+              key={foodItem.foodItemName}
+              title={foodItem.foodItemName}
+              dateCreated={foodItem.dateAdded}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+  return <div>No food items detected</div>;
 }
 
 export default FoodItems;
