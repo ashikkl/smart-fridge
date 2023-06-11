ff546ac4083db134aa1b66109f81c433281c1c63
Ashik K L <ashikkl19112001@gmail.com>
2023-06-11 12:31:23 +0530

    fix: api route syntax

--- src/app/api/fetch/inside/route.ts
+++ src/app/api/fetch/inside/route.ts
@@ -2,7 +2,7 @@ import { getStorage, ref, getDownloadURL } from "firebase/storage";
 import { initializeApp } from "firebase/app";
 import { NextResponse } from "next/server";
 
-export const firebaseConfig = {
+const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
