515aed9f69507f9ef4d707fb9ddb0af7fce312de
Ashik K L <ashikkl19112001@gmail.com>
2023-06-15 23:21:10 +0530

    update: make some adjustments

--- src/components/ClassifyImage.tsx
+++ src/components/ClassifyImage.tsx
@@ -1,47 +1,15 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { FoodItem, useFoodItemStore } from "./FoodItemsStore";
+import { useEffect } from "react";
+import { useFoodItemStore } from "./FoodItemsStore";
 import { useFridgeStore } from "./FridgeSelector";
 import axios from "axios";
 import axiosRetry from "axios-retry";
 
 function ClassifyImage() {
-  const [imageURL, setImageURL] = useState<string>("");
   const { fridge, updateURL } = useFridgeStore();
   const fridgeId = fridge;
-  useEffect(() => {
-    const fetchImageURL = async () => {
-      let config = {
-        timeout: 30000,
-        method: "get",
-        maxBodyLength: Infinity,
-        url: `/api/fetch/inside`,
-        params: { fridgeId: fridgeId },
-        headers: {},
-      };
-
-      const client = axios.create(config);
-      axiosRetry(client, {
-        retries: 3,
-        retryDelay: axiosRetry.exponentialDelay,
-      });
-      const ImageReq = await client
-        .request(config)
-        .then((response) => {
-          const url = response.data.url;
-          setImageURL(url);
-          updateURL(url);
-        })
-        .catch((err) => {
-          console.error("Error fetching image url from server:", err);
-        });
-    };
-
-    fetchImageURL();
-  }, [fridgeId, updateURL]);
   const { foodItems, updateFoodItems } = useFoodItemStore();
-  const [newFoodItems, setFoodItems] = useState<FoodItem[]>([]);
 
   function notInList(item: string) {
     if (foodItems.length === 0) return true;
@@ -59,67 +27,99 @@ function ClassifyImage() {
     return result;
   }
 
-  const raw = JSON.stringify({
-    user_app_id: {
-      user_id: "clarifai",
-      app_id: "main",
-    },
-    inputs: [
-      {
-        data: {
-          image: {
-            url: imageURL,
-          },
-        },
-      },
-    ],
-  });
-
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      Authorization: "Key " + "eb298c4d60804755903fa44945fda330",
-    },
-    body: raw,
-  };
-
   useEffect(() => {
-    async function fetchFoodItems() {
-      await fetch(
-        `https://api.clarifai.com/v2/models/food-item-recognition/versions/1d5fd481e0cf4826aa72ec3ff049e044/outputs`,
-        requestOptions
-      )
-        .then((response) => response.json())
-        .then((result) => {
-          const out = result.outputs[0].data.concepts;
-          let newFoodItems = out.map((foodItem: any) => {
-            let checker = notInList(foodItem.name);
+    const fetchImageURL = async () => {
+      try {
+        const config = {
+          timeout: 30000,
+          method: "get",
+          maxBodyLength: Infinity,
+          url: `/api/fetch/inside`,
+          params: { fridgeId: fridgeId },
+          headers: {},
+        };
 
-            if (checker && foodItem.value > 0.005) {
-              return {
-                foodItemName: foodItem.name,
-                dateAdded: new Date(),
-              };
-            }
-            if (!checker) {
-              let oldDate = getDate(foodItem.name);
+        const client = axios.create(config);
+        axiosRetry(client, {
+          retries: 3,
+          retryDelay: axiosRetry.exponentialDelay,
+        });
 
-              return { foodItemName: foodItem.name, dateAdded: oldDate };
-            }
-            return null;
-          });
-          newFoodItems = newFoodItems.filter((item: any) => item !== null);
+        const response = await client.request(config);
+        const url = response.data.url;
+        updateURL(url);
 
-          setFoodItems(newFoodItems);
-          updateFoodItems(newFoodItems);
-        })
-        .catch((error) => console.log("error", error));
-    }
+        const raw = JSON.stringify({
+          user_app_id: {
+            user_id: "clarifai",
+            app_id: "main",
+          },
+          inputs: [
+            {
+              data: {
+                image: {
+                  url: url,
+                },
+              },
+            },
+          ],
+        });
 
-    fetchFoodItems();
+        const requestOptions = {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            Authorization: "Key " + "eb298c4d60804755903fa44945fda330",
+          },
+          body: raw,
+        };
+
+        const fetchFoodItems = async () => {
+          try {
+            const response = await fetch(
+              `https://api.clarifai.com/v2/models/food-item-recognition/versions/1d5fd481e0cf4826aa72ec3ff049e044/outputs`,
+              requestOptions
+            );
+            const result = await response.json();
+
+            const out = result.outputs[0].data.concepts;
+            console.log(out);
+            
+            let newFoodItems = out.map((foodItem: any) => {
+              let checker = notInList(foodItem.name);
+
+              if (checker && foodItem.value > 0.5) {
+                return {
+                  foodItemName: foodItem.name,
+                  dateAdded: new Date(),
+                };
+              }
+              if (!checker) {
+                let oldDate = getDate(foodItem.name);
+
+                return { foodItemName: foodItem.name, dateAdded: oldDate };
+              }
+              return null;
+            });
+            newFoodItems = newFoodItems.filter((item: any) => item !== null);
+
+            updateFoodItems(newFoodItems);
+          } catch (error) {
+            console.log("error", error);
+          }
+        };
+
+        if (url.length) {
+          await fetchFoodItems();
+        }
+      } catch (err) {
+        console.error("Error fetching food item data from server:", err);
+      }
+    };
+
+    fetchImageURL();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [fridgeId]);
 
   return <div />;
 }
